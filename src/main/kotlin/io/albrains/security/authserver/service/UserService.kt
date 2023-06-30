3ec49191f5d4e8c19fdcced0dfc3bc7db4f4d154
Alex SIEYADJEU <alex.sieyadjeu@lyaprotect.com>
2023-06-30 08:24:35 +0200

    change signature exception

--- src/main/kotlin/io/albrains/security/authserver/service/UserService.kt
+++ src/main/kotlin/io/albrains/security/authserver/service/UserService.kt
@@ -7,6 +7,6 @@ import reactor.core.publisher.Mono
 class UserService {
     fun findByUsernameAndPassword(username: String, password: String): Mono<UserApp> {
         return Mono.just(UserApp("user", "pass"))
-            .switchIfEmpty(Mono.error(Exception("user $username not found")))
+            .switchIfEmpty(Mono.error(Exception("Username or password not found")))
     }
 }
